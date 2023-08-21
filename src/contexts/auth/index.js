efa25007d77ff7a16ffbe5ee5a0938b06ce53ea5
Jenna C <jennac0312@gmail.com>
2023-08-21 12:08:30 -0400

    fixed typos

--- src/contexts/auth/index.js
+++ src/contexts/auth/index.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useMemo } from "react";
-// use memo similar to use satte. saves value and saves a funcitona dn basically if the val doesnt change and the page rerenders it doesn re run the snippet that takes longer. its supposed to speed up the process - dylan
+// use memo similar to use state. saves value and saves a funciton and basically if the val doesnt change and the page rerenders it doesnt re run the snippet that takes longer. its supposed to speed up the process - dylan
 import { useCookies } from 'react-cookie' // cookies to save to our browser
 import axios from "axios";
 
@@ -11,11 +11,11 @@ export const UserProvider = ( { children } ) => {
 
     const [ cookies, setCookies, removeCookie ] = useCookies()
     // const [ save all cookies using for AppContext, set certain variable as cookies... , logging out is removing cookies. singluar bc usually we remove one cookie at a time ]
-    // manage our cookies [ variable that saves cookies... under cookie setting, like out setstate (jwt webt token? to log us in), remove cookies.. from website/app so someone else can log in ( remove jwt ) ]
+    // manage our cookies [ variable that saves cookies... under cookie setting, like our setState (jwt web token? to log us in), remove cookies.. from website/app so someone else can log in ( remove jwt ) ]
 
     // all functions to access our backend.. so async
     const login = async ( formData ) => {
-        // need parameter form data which is prob username and password
+        // need parameter form data ( username and password )
 
         let response = await axios({ 
             // all from auth.js router.post for login and authentication
@@ -26,17 +26,17 @@ export const UserProvider = ( { children } ) => {
 
             // post req to this route that sends this data
 
-         }) // object that has all info that we want to provide for the axious api request... in this cause our api is our backend
+        }) // object that has all info that we want to provide for the axious api request... in this case our api is our backend
 
         //  would you like to accept cookies from this website?
-        // set cookies as the token
+        // set cookies as the 'token' key:value
         setCookies( 'token', response.data.token ) // saving token response in "token" ... this is your token for logging in
         // key value { token: response.data.token }
     }
 
     // bring in formData from our form on the front end
     const signUp = async ( formData ) => {
-        // sign up is on /api/users route.. signup is a POST route. validates info brought in, try to acces DB if no new user we create one, and responds with webtoken
+        // sign up is on /api/users route.. signup is a POST route. validates info brought in, try to access DB if no new user we create one, and responds with webtoken (this all comes from backend)
 
         let response = await axios({
             method: 'POST',
@@ -53,6 +53,7 @@ export const UserProvider = ( { children } ) => {
 
     // we are creating a json web token each time
 
+    
     const logOut = () => {
         [ 'token' ].forEach(( obj ) => {
             removeCookie( obj )
